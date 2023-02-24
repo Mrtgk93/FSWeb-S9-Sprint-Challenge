b6b1b0b02d2bfcb647a845860840bfc0ccc1c124
Mrtgk93 <mrtgk93@hotmail.com>
2023-02-24 22:24:36 +0300

    ödev tamamlandı

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,155 @@
-import React from 'react'
+import axios from "axios";
+import { response } from "msw";
+import React, { useState } from "react";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
 
-  function getXY() {
-    // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
-    // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
-  }
-
-  function getXYMesaj() {
-    // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
-    // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
-  }
+  const [initialMessage, setInitialMessage] = useState("");
+  const [initialEmail, setInitialEmail] = useState("");
+  const [initialSteps, setInitialSteps] = useState(0);
+  const [initialIndex, setInitialIndex] = useState(4);
+  const [kordinat, setKordinat] = useState([2, 2]);
 
   function reset() {
+    setInitialIndex(4);
+    setInitialMessage("");
+    setInitialSteps(0);
+    setInitialEmail("");
+    setKordinat([2, 2]);
+
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
   }
 
-  function sonrakiIndex(yon) {
-    // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
-    // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
-    // şu anki indeksi değiştirmemeli.
-  }
+  const moveUp = () => {
+    setInitialMessage("");
 
-  function ilerle(evt) {
-    // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
-    // ve buna göre state i değiştirir.
-  }
+    if (initialIndex > 2) {
+      // Eğer simge üst kenarda değilse
+      setInitialIndex(initialIndex - 3);
+      // currentPosition'ı 3 azaltarak simgeyi yukarı hareket ettirin
+      setInitialSteps(initialSteps + 1);
+      setKordinat([kordinat[0], kordinat[1] - 1]);
+    } else {
+      setInitialMessage("Yukarıya gidemezsiniz");
+    }
+  };
+  const moveDown = () => {
+    setInitialMessage("");
+
+    if (initialIndex < 6) {
+      // Eğer simge alt kenarda değilse
+      setInitialIndex(initialIndex + 3);
+      // currentPosition'ı 3 artırarak simgeyi aşağı hareket ettirin
+      setInitialSteps(initialSteps + 1);
+      setKordinat([kordinat[0], kordinat[1] + 1]);
+    } else {
+      setInitialMessage("Aşağıya gidemezsiniz");
+    }
+  };
+  const moveLeft = () => {
+    setInitialMessage("");
+
+    if (initialIndex % 3 > 0) {
+      // Eğer simge sol kenarda değilse
+      setInitialIndex(initialIndex - 1);
+      // currentPosition'ı 1 azaltarak simgeyi sola hareket ettirin
+      setInitialSteps(initialSteps + 1);
+      setKordinat([kordinat[0] - 1, kordinat[1]]);
+    } else {
+      setInitialMessage("Sola gidemezsiniz");
+    }
+  };
+  const moveRight = () => {
+    setInitialMessage("");
+
+    if (initialIndex % 3 < 2) {
+      // Eğer simge sağ kenarda değilse
+      setInitialIndex(initialIndex + 1);
+      // currentPosition'ı 1 artırarak simgeyi sağa hareket ettirin
+      setInitialSteps(initialSteps + 1);
+      setKordinat([kordinat[0] + 1, kordinat[1]]);
+    } else {
+      setInitialMessage("Sağa gidemezsiniz");
+    }
+  };
 
   function onChange(evt) {
     // inputun değerini güncellemek için bunu kullanabilirsiniz
+    setInitialEmail(evt.target.value);
   }
 
   function onSubmit(evt) {
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    evt.preventDefault();
+
+    axios
+      .post("http://localhost:9000/api/result", {
+        x: kordinat[0],
+        y: kordinat[1],
+        steps: initialSteps,
+        email: initialEmail,
+      })
+      .then((response) => setInitialMessage(response.data.message))
+      .catch((error) => setInitialMessage(error.response.data.message));
+    setInitialEmail("");
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">
+          Koordinatlar ({kordinat[0]},{kordinat[1]})
+        </h3>
+        <h3 id="steps">{initialSteps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div
+            key={idx}
+            className={`square${idx === initialIndex ? " active" : ""}`}
+          >
+            {idx === initialIndex ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{initialMessage}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={moveLeft} id="left">
+          SOL
+        </button>
+        <button onClick={moveUp} id="up">
+          YUKARI
+        </button>
+        <button onClick={moveRight} id="right">
+          SAĞ
+        </button>
+        <button onClick={moveDown} id="down">
+          AŞAĞI
+        </button>
+        <button onClick={reset} id="reset">
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          id="email"
+          type="email"
+          placeholder="email girin"
+          value={initialEmail}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
